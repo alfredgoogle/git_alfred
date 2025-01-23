28de388971c4401b106707394b898339da4db3bc
肖文章 <xiaowz@ersoft.cn>
2025-01-23 11:48:36 +0800

    fix capter4

--- 读书笔记/vue 设计/capter1.js
+++ 读书笔记/vue 设计/capter1.js
@@ -15,3 +15,73 @@ function render(obj,root){
     }
     root.appendChild(ele)
 }
+
+
+ const vnode = {
+       tag: 'div',
+       props: {
+         onClick: () => alert('hello')
+       },
+       children: 'click me'
+     }
+
+ function renderer(vnode, container) {
+    // 使用 vnode.tag 作为标签名称创建 DOM 元素
+    const el = document.createElement(vnode.tag)
+    // 遍历 vnode.props，将属性、事件添加到 DOM 元素
+    for (const key in vnode.props) {
+        if (/^on/.test(key)) {
+        // 如果 key 以 on 开头，说明它是事件
+        el.addEventListener(
+            key.substr(2).toLowerCase(), // 事件名称 onClick ---> click
+            vnode.props[key] // 事件处理函数
+        )
+        }
+    }
+
+    // 处理 children
+    if (typeof vnode.children === 'string') {
+        // 如果 children 是字符串，说明它是元素的文本子节点
+        el.appendChild(document.createTextNode(vnode.children))
+    } else if (Array.isArray(vnode.children)) {
+        // 递归地调用 renderer 函数渲染子节点，使用当前元素 el 作为挂载点
+        vnode.children.forEach(child => renderer(child, el))
+    }
+
+    // 将元素添加到挂载点下
+    container.appendChild(el)
+}
+ function mountComponent(vnode, container) {
+    // vnode.tag 是组件对象，调用它的 render 函数得到组件要渲染的内容（虚拟 DOM）
+    const subtree = vnode.tag.render()
+    // 递归地调用 renderer 渲染 subtree
+    renderer(subtree, container)
+}
+
+ function mountElement(vnode, container) {
+    // 使用 vnode.tag 作为标签名称创建 DOM 元素
+    const el = document.createElement(vnode.tag)
+    // 遍历 vnode.props，将属性、事件添加到 DOM 元素
+    for (const key in vnode.props) {
+        if (/^on/.test(key)) {
+        // 如果 key 以字符串 on 开头，说明它是事件
+        el.addEventListener(
+            key.substr(2).toLowerCase(), // 事件名称 onClick ---> click
+            vnode.props[key] // 事件处理函数
+        )
+        }
+    }
+
+    // 处理 children
+    if (typeof vnode.children === 'string') {
+        // 如果 children 是字符串，说明它是元素的文本子节点
+        el.appendChild(document.createTextNode(vnode.children))
+    } else if (Array.isArray(vnode.children)) {
+        // 递归地调用 renderer 函数渲染子节点，使用当前元素 el 作为挂载点
+        vnode.children.forEach(child => renderer(child, el))
+    }
+
+    // 将元素添加到挂载点下
+    container.appendChild(el)
+}
+
---
+++ 读书笔记/vue 设计/capter4.js
@@ -0,0 +1,60 @@
+
+const bucket = new WeakMap();
+
+const data = {text:'hello world'};
+
+let activeEffect;
+
+const obj = new Proxy(data,{
+    get(target,key){
+        track(target,key)
+        return target[key];
+    },
+    set(target,key,newValue){
+        target[key] = newValue;
+        trigger(target,key);
+        return true;
+    }
+})
+
+function track(target,key){
+    if(activeEffect){
+        // bucket.add(activeEffect);
+        let depsMap = bucket.get(target);
+        if(!depsMap){
+            bucket.set(target, depsMap = new Map());
+        }else{
+            let deps = depsMap.get(key);
+            if(!deps){
+                depsMap.set(key, deps = new Set());
+            }else{
+                deps.add(activeEffect);
+            }
+        }
+    }
+}
+
+function trigger(target,key){
+    let depsMap = bucket.get(target);
+    if(depsMap){
+        let deps = depsMap.get(key);
+        if(deps){
+            deps.forEach(fn=>fn())
+        }
+    }
+}
+
+function effect(fn){
+    let effectFn = (fn){
+        activeEffect = effectFn;
+        fn();
+    }
+    effectFn.deps = [];
+    effectFn();
+}
+
+
+effect(()=>{
+    console.log('effect run');
+    document.body.innerHtml = data.text;
+})
